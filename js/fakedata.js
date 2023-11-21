7475f5d763b2744bcbea5d6e6a6281078bfac3e6
jonatan.arnandis <jonatan.arnandis@brandmanic.com>
2023-11-21 14:48:30 +0100

    commercial api

---
+++ js/fakedata.js
@@ -0,0 +1,263 @@
+export const queryUserInfoResponse = {
+  open_id: "1",
+  union_id: "2",
+  avatar_url: "http://...",
+  avatar_url100: "http://...",
+  avatar_large_url: "http://...",
+  display_name: "Toñito",
+  bio_description: "El mejor Toñito de calle 🔥🔥",
+  profile_deep_link: "http://...",
+  is_verified: "1",
+  follower_count: "1234",
+  following_count: "1234",
+  likes_count: "54321",
+  video_count: "98765",
+};
+export const queryVideosResponse = {
+  data: {
+    videos: [
+      {
+        title: "Video 1",
+        id: "1234123412345678567",
+      },
+      {
+        title: "Video 2",
+        id: "1010102020203030303",
+      },
+    ],
+  },
+  error: {
+    code: "ok",
+    message: "",
+    log_id: "20220829194722CBE87ED59D524E727021",
+  },
+};
+
+export const queryAdsResponse = {
+  data: {
+    ads: [
+      {
+        ad: {
+          first_shown_date: 20210101,
+          id: 1,
+          image_urls: [
+            "https://asdfcdn.com/17392712.jpeg?x-expires=1679169600\u0026x-signature=asdf",
+          ],
+          last_shown_date: 20210101,
+          status: "active",
+          videos: [
+            {
+              url: "https://asdfcdn.com/..../127364jmdfjsa93d8cn30dm2di/?mime_type=video_mp4",
+            },
+            {
+              url: "https://asdfcdn.com/..../1kmeidhfb38u21nd82hsk389fd/?mime_type=video_mp4",
+            },
+          ],
+          reach: {
+            unique_user_seen: "11K",
+          },
+        },
+        advertiser: {
+          buisness_id: 3847236290405,
+          business_name: "Awe Food Co.",
+          paid_by: "Awe Co.",
+        },
+      },
+      {
+        ad: {
+          first_shown_date: 20210101,
+          id: 2,
+          image_urls: [
+            "https://asdfcdn.com/17392712.jpeg?x-expires=1679169600\u0026x-signature=asdf",
+          ],
+          last_shown_date: 20210101,
+          status: "active",
+          videos: [
+            {
+              url: "https://asdfcdn.com/..../127364jmdfjsa93d8cn30dm2di/?mime_type=video_mp4",
+            },
+            {
+              url: "https://asdfcdn.com/..../1kmeidhfb38u21nd82hsk389fd/?mime_type=video_mp4",
+            },
+          ],
+          reach: {
+            unique_user_seen: "11K",
+          },
+        },
+        advertiser: {
+          buisness_id: 3847236290405,
+          business_name: "Awe Food Co.",
+          paid_by: "Awe Co.",
+        },
+      },
+    ],
+    has_more: "true",
+    search_id: "2837438294054038",
+  },
+  error: {
+    code: "ok",
+    http_status_code: 200,
+    log_id: "202304280326050102231031430C7E754E",
+    message: "",
+  },
+};
+export const queryAdvertisersResponse = {
+  data: {
+    advertisers: [
+      {
+        business_id: 1755645247067185,
+        business_name: "Awesome Food Co.",
+        country_code: "US",
+      },
+      {
+        business_id: 183746395837294,
+        business_name: "Awesome Drink Co.",
+        country_code: "CA",
+      },
+    ],
+  },
+  error: {
+    code: "ok",
+    http_status_code: 200,
+    log_id: "202207280326050102231031430C7E754E",
+    message: "",
+  },
+};
+
+export const queryAdDetailResponse = {
+  data: {
+    ad: {
+      first_shown_date: 20210101,
+      id: 1923845247192304,
+      image_urls: [
+        "https://111.com/17392712.jpeg?x-expires=1679169600\u0026x-signature=asdf",
+        "https://2222.com/17392712.jpeg?x-expires=1679169600\u0026x-signature=asdf",
+      ],
+      last_shown_date: 20210101,
+      status: "active",
+      videos: [
+        {
+          url: "https://asdfcdn.com/..../127364jmdfjsa93d8cn30dm2di/?mime_type=video_mp4",
+        },
+        {
+          url: "https://asdfcdn.com/..../1kmeidhfb38u21nd82hsk389fd/?mime_type=video_mp4",
+        },
+      ],
+      reach: {
+        unique_users_seen: "30K",
+        unique_users_seen_by_country: {
+          GB: "18K",
+          IT: "12K",
+        },
+      },
+    },
+    ad_group: {
+      target: {
+        number_of_users_targeted: "50K",
+        country: ["IT", "GB"],
+        age: {
+          "13-17": false,
+          "18-24": false,
+          "25-34": false,
+          "35-44": true,
+          "35-44": true,
+          "55+": true,
+        },
+        gender: {
+          female: true,
+          male: true,
+          other_genders: true,
+        },
+        audience_targeting: "No",
+        video_interactions: "Entertainment",
+        creator_interactions: "Talent",
+        interest: "News & Entertainment, Business Services",
+      },
+    },
+    advertiser: {
+      business_id: 1755645247067185,
+      business_name: "Awesome Co.",
+      country_code: "US",
+      paid_by: "Awesome Co.",
+      tiktok_account: {
+        avatar_url:
+          "https://asdf.tiktokcdn.com/1736254.jpeg?x-expires=1679169600\u0026x-signature=asdf",
+        follower_count: 26374,
+        profile_url: "https://www.tiktok.com/@awesome_co",
+      },
+    },
+  },
+  error: {
+    code: "ok",
+    http_status_code: 200,
+    log_id: "202207280326050102231031430C7E754E",
+    message: "",
+  },
+};
+export const queryAdReportResponse = {
+  data: {
+    count_time_series_by_country: {
+      IT: [
+        { date: "20210109", count: 45 },
+        { date: "20210108", count: 24 },
+      ],
+      ES: [
+        { date: "20210109", count: 48 },
+        { date: "20210108", count: 22 },
+      ],
+    },
+  },
+  error: {
+    code: "ok",
+    http_status_code: 200,
+    log_id: "202207280326050102231031430C7E754E",
+    message: "",
+  },
+};
+export const queryCommercialContentResponse = {
+  data: {
+    commercial_contents: [
+      {
+        brand_names: ["My Awesome Co.", "HelloWorld Inc."],
+        create_date: "20230109",
+        create_timestamp: 1696875852,
+        creator: {
+          username: "joe1234567",
+        },
+        id: "v09044g40000ce6enu3c77u36l73sp20",
+        label: "Paid partnership",
+        videos: [
+          {
+            id: 1,
+            status: true,
+            cover_image_url:
+              "https://asdfcdn.com/17392712.jpeg?x-expires=1679169600\u0026x-signature=asdf",
+            url: "https://www.tiktok.com/@joe1234567/video/19384729204821234",
+          },
+          {
+            id: 2,
+            status: true,
+            cover_image_url:
+              "https://asdfcdn.com/17392712.jpeg?x-expires=1679169600\u0026x-signature=asdf",
+            url: "https://www.tiktok.com/@joe1234567/video/19384729204821234",
+          },
+          {
+            id: 3,
+            status: false,
+            cover_image_url:
+              "https://asdfcdn.com/17392712.jpeg?x-expires=1679169600\u0026x-signature=asdf",
+            url: "https://www.tiktok.com/@joe1234567/video/19384729204821234",
+          },
+        ],
+      },
+    ],
+    has_more: "true",
+    search_id: "2837438294054038",
+  },
+  error: {
+    code: "ok",
+    http_status_code: 200,
+    log_id: "202207280326050102231031430C7E754E",
+    message: "",
+  },
+};
--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
+import {
+  queryUserInfoResponse,
+  queryVideosResponse,
+  queryAdsResponse,
+  queryAdvertisersResponse,
+  queryAdDetailResponse,
+  queryAdReportResponse,
+  queryCommercialContentResponse,
+} from "./fakedata.js";
 const options = {
   method: "POST",
   headers: {
@@ -25,6 +34,30 @@ $(document).ready(function () {
 
   let videoData = fetchData("fake", options, queryVideosResponse);
   videoData.then((res) => renderVideoData(res));
+
+  //https://open.tiktokapis.com/v2/research/adlib/ad/query/
+  let adData = fetchData("", options, queryAdsResponse);
+  adData.then((res) => renderAdData(res));
+
+  //https://open.tiktokapis.com/v2/research/adlib/advertiser/query/
+  let advertiserData = fetchData("", options, queryAdvertisersResponse);
+  advertiserData.then((res) => renderAdvertiserData(res));
+
+  //https://open.tiktokapis.com/v2/research/adlib/ad/detail
+  let adDetailsData = fetchData("", options, queryAdDetailResponse);
+  adDetailsData.then((res) => renderAdDetailData(res));
+
+  //https://open.tiktokapis.com/v2/research/adlib/ad/report/
+  let adReportData = fetchData("", options, queryAdReportResponse);
+  adReportData.then((res) => renderAdReportData(res));
+
+  //https://open.tiktokapis.com/v2/research/adlib/commercial_content/report/
+  let commercialContentData = fetchData(
+    "",
+    options,
+    queryCommercialContentResponse
+  );
+  commercialContentData.then((res) => renderCommercialContentData(res));
 });
 
 const fetchData = (url, options, fakeJson) => {
@@ -62,37 +95,218 @@ const renderVideoData = (json) => {
   }
 };
 
-const queryUserInfoResponse = {
-  open_id: "1",
-  union_id: "2",
-  avatar_url: "http://...",
-  avatar_url100: "http://...",
-  avatar_large_url: "http://...",
-  display_name: "Toñito",
-  bio_description: "El mejor Toñito de calle 🔥🔥",
-  profile_deep_link: "http://...",
-  is_verified: "1",
-  follower_count: "1234",
-  following_count: "1234",
-  likes_count: "54321",
-  video_count: "98765",
+const renderAdData = (json) => {
+  let th = `<tr style="border: 1px solid black; padding: 0.5rem">`;
+  let td = `<tr style="border: 1px solid black;padding: .5rem;">`;
+  let tdMockup = `<td style="border: 1px solid black;padding: .5rem;">`;
+  let tdMockupEnd = `</td>`;
+  let ads = json.data.ads;
+  let once = false;
+  for (const adobj of ads) {
+    const { ad, advertiser } = adobj;
+
+    for (const key in ad) {
+      if (!once) {
+        th += `<th style="background-color: #000000; color: white; padding: 0.5rem">${key}</th>`;
+      }
+      if (key == "videos") {
+        td += tdMockup;
+        for (const video of ad[key]) {
+          const { url } = video;
+          td += url + "</br>";
+        }
+        td += tdMockupEnd;
+      } else if (key == "image_urls") {
+        td += tdMockup;
+        for (const img of ad[key]) {
+          td += img;
+        }
+        td += tdMockupEnd;
+      } else if (key == "reach") {
+        td += tdMockup + ad[key]["unique_user_seen"] + tdMockupEnd;
+      } else {
+        td += tdMockup + ad[key] + tdMockupEnd;
+      }
+    }
+    for (const key in advertiser) {
+      if (!once) {
+        th += `<th style="background-color: #000000; color: white; padding: 0.5rem">${key}</th>`;
+      }
+      td += tdMockup + advertiser[key] + tdMockupEnd;
+    }
+    if (!once) {
+      th += "</tr>";
+      $("#contenido__api--tablaAds").append(th);
+    }
+    once = true;
+
+    td += "</tr>";
+  }
+  $("#contenido__api--tablaAds").append(td);
 };
-const queryVideosResponse = {
-  data: {
-    videos: [
-      {
-        title: "Video 1",
-        id: "1234123412345678567",
-      },
-      {
-        title: "Video 2",
-        id: "1010102020203030303",
-      },
-    ],
-  },
-  error: {
-    code: "ok",
-    message: "",
-    log_id: "20220829194722CBE87ED59D524E727021",
-  },
+
+const renderAdvertiserData = (json) => {
+  let th = `<tr style="border: 1px solid black; padding: 0.5rem">`;
+  let td = `<tr style="border: 1px solid black;padding: .5rem;">`;
+  let tdMockup = `<td style="border: 1px solid black;padding: .5rem;">`;
+  let tdMockupEnd = `</td>`;
+  let advs = json.data.advertisers;
+  let once = false;
+  for (const adv of advs) {
+    for (const key in adv) {
+      if (!once) {
+        th += `<th style="background-color: #000000; color: white; padding: 0.5rem">${key}</th>`;
+      }
+      td += tdMockup + adv[key] + tdMockupEnd;
+    }
+    if (!once) {
+      th += "</tr>";
+      $("#contenido__api--tablaAdvertisers").append(th);
+    }
+    once = true;
+
+    td += "</tr>";
+  }
+
+  $("#contenido__api--tablaAdvertisers").append(td);
+};
+const renderAdDetailData = (json) => {
+  let th = `<tr style="border: 1px solid black; padding: 0.5rem">`;
+  let td = `<tr style="border: 1px solid black;padding: .5rem;">`;
+  let tdMockup = `<td style="border: 1px solid black;padding: .5rem;">`;
+  let tdMockupEnd = `</td>`;
+  let thMockup = `<th style="background-color: #000000; color: white; padding: 0.5rem">`;
+  let thMockupEnd = `</th>`;
+  let { ad, ad_group, advertiser } = json.data;
+
+  for (const key in ad) {
+    if (key == "videos") {
+      th += thMockup + key + thMockupEnd;
+      td += tdMockup;
+      for (const video of ad[key]) {
+        const { url } = video;
+        td += url + "</br>";
+      }
+      td += tdMockupEnd;
+    } else if (key == "image_urls") {
+      th += thMockup + key + thMockupEnd;
+      td += tdMockup;
+      for (const img of ad[key]) {
+        td += img;
+      }
+      td += tdMockupEnd;
+    } else if (key == "reach") {
+      th += thMockup + "unique_users_seen" + thMockupEnd;
+
+      td += tdMockup + ad[key]["unique_users_seen"] + tdMockupEnd;
+      for (const reachKey in ad[key]["unique_users_seen_by_country"]) {
+        th += thMockup + reachKey + thMockupEnd;
+
+        td +=
+          tdMockup +
+          ad[key]["unique_users_seen_by_country"][reachKey] +
+          tdMockupEnd;
+      }
+    } else {
+      th += thMockup + key + thMockupEnd;
+      td += tdMockup + ad[key] + tdMockupEnd;
+    }
+  }
+  let target = ad_group.target;
+  for (const key in target) {
+    if (key == "age" || key == "gender") {
+      for (const subKey in target[key]) {
+        th += thMockup + subKey + thMockupEnd;
+
+        td += tdMockup + target[key][subKey] + tdMockupEnd;
+      }
+    } else {
+      th += thMockup + key + thMockupEnd;
+      td += tdMockup + target[key] + tdMockupEnd;
+    }
+  }
+
+  for (const key in advertiser) {
+    if (key == "tiktok_account") {
+      for (const subKey in advertiser[key]) {
+        th += thMockup + subKey + thMockupEnd;
+
+        td += tdMockup + advertiser[key][subKey] + tdMockupEnd;
+      }
+    } else {
+      th += thMockup + key + thMockupEnd;
+      td += tdMockup + advertiser[key] + tdMockupEnd;
+    }
+  }
+
+  th += "</tr>";
+
+  td += "</tr>";
+  $("#contenido__api--tablaAdDetails").append(th);
+  $("#contenido__api--tablaAdDetails").append(td);
+};
+const renderAdReportData = (json) => {
+  let th = `<tr style="border: 1px solid black; padding: 0.5rem">`;
+  let td = `<tr style="border: 1px solid black;padding: .5rem;">`;
+  let tdMockup = `<td style="border: 1px solid black;padding: .5rem;">`;
+  let tdMockupEnd = `</td>`;
+  let report = json.data.count_time_series_by_country;
+
+  for (const key in report) {
+    th += `<th style="background-color: #000000; color: white; padding: 0.5rem">${key}</th>`;
+    let text = "";
+    for (let data of report[key]) {
+      text += data.date + " " + data.count + " </br>";
+    }
+    td += tdMockup + text + tdMockupEnd;
+  }
+  th += "</tr>";
+  $("#contenido__api--tablaAdReport").append(th);
+
+  td += "</tr>";
+
+  $("#contenido__api--tablaAdReport").append(td);
+};
+const renderCommercialContentData = (json) => {
+  let th = `<tr style="border: 1px solid black; padding: 0.5rem">`;
+  let td = `<tr style="border: 1px solid black;padding: .5rem;">`;
+  let tdMockup = `<td style="border: 1px solid black;padding: .5rem;">`;
+  let tdMockupEnd = `</td>`;
+  let thMockup = `<th style="background-color: #000000; color: white; padding: 0.5rem">`;
+  let thMockupEnd = `</th>`;
+  let commercialcontents = json.data.commercial_contents;
+  for (let cc of commercialcontents) {
+    for (let key in cc) {
+      if (key == "creator") {
+        for (const subKey in cc[key]) {
+          th += thMockup + subKey + thMockupEnd;
+
+          td += tdMockup + cc[key][subKey] + tdMockupEnd;
+        }
+      } else if (key == "videos") {
+        th += thMockup + "videos" + thMockupEnd;
+        let text = "";
+        for (let v of cc[key]) {
+          text +=
+            v.id +
+            " " +
+            v.status +
+            " " +
+            v.cover_image_url +
+            " " +
+            v.url +
+            " </br>";
+        }
+        td += tdMockup + text + tdMockupEnd;
+      } else {
+        th += thMockup + key + thMockupEnd;
+        td += tdMockup + cc[key] + tdMockupEnd;
+      }
+    }
+  }
+  th += "</tr>";
+
+  td += "</tr>";
+  $("#contenido__api--tablaCommercialContent").append(th);
+  $("#contenido__api--tablaCommercialContent").append(td);
 };
