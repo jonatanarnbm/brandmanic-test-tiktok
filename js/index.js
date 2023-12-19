270354361985157e26118c82e1ef0b9d96260de8
ivansaezbm <150167924+ivansaezbm@users.noreply.github.com>
2023-12-19 15:30:54 +0000

    fetch desde el back http://localhost

--- js/index.js
+++ js/index.js
@@ -97,7 +97,7 @@ $(document).ready(function () {
   /* Query User Info - Display API */
   $("#queryUser__boton--buscar").on("click", () => {
     fieldsSelected = getFieldsClass(".fieldUser__checkbox");
-    let UserData = fetch(`127.0.0.1:3000/test`, {
+    let UserData = fetch(`http://localhost:3000/test`, {
       fields: fieldsSelected,
       token:localStorage.getItem('access_token')
     })
