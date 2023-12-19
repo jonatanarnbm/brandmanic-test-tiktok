7826d1c6871764154a2b1cfa85fd4dba9713df83
jonatan.arnandis <jonatan.arnandis@brandmanic.com>
2023-12-19 11:53:22 +0100

    research api

--- routes.js
+++ routes.js
@@ -1,4 +1,5 @@
 module.exports = function (app) {
   app.use("/login", require("./routes/login"));
   app.use("/display", require("./routes/displayapi"));
+  app.use("/research", require("./routes/researchapi"));
 };
---
+++ routes/researchapi.js
@@ -0,0 +1,72 @@
+var express = require("express");
+var router = express.Router();
+
+router.get("/user", async function (req, res, next) {
+  const { fields, token, username } = req.query;
+  console.log(req.query);
+  let url = "https://open.tiktokapis.com/v2/research/user/info/";
+  url += "?fields=" + fields.toString();
+  let body = {};
+  if (username != null) {
+    body = { ...body, username };
+  }
+  let test = await fetch(url, {
+    method: "POST",
+    headers: {
+      Authorization: "Bearer " + token,
+      "Content-Type": "application/json",
+    },
+    body,
+  });
+
+  test.json().then((j) => res.json(j));
+});
+
+router.get("/video", async function (req, res, next) {
+  let token = "Bearer " + req.query.token;
+  let fields = req.query.fields;
+  let videoIds = req.query.videoIds;
+  let url = "https://open.tiktokapis.com/v2/research/video/query/";
+
+  url += "?fields=" + fields.toString();
+  let body = {};
+  if (videoIds != null) {
+    body = { filters: { video_ids: [videoIds] } };
+  }
+  let test = await fetch(url, {
+    method: "POST",
+    headers: {
+      Authorization: token,
+      "Content-Type": "application/json",
+    },
+    body,
+  });
+
+  test.json().then((j) => res.json(j));
+});
+router.get("/video/list", async function (req, res, next) {
+  const { max_count, cursor, fields, token } = req.query;
+
+  let url = "https://open.tiktokapis.com/v2/video/list/";
+
+  url += "?fields=" + fields.toString();
+  let body = {};
+  if (max_count != null) {
+    body = { ...body, max_count };
+  }
+  if (cursor != null) {
+    body = { ...body, cursor };
+  }
+
+  let test = await fetch(url, {
+    method: "POST",
+    headers: {
+      Authorization: "Bearer " + token,
+      "Content-Type": "application/json",
+    },
+    body,
+  });
+
+  test.json().then((j) => res.json(j));
+});
+module.exports = router;
