66fb469fae504c70104b567ef47ed7322daced32
Ivan Saez <ivan.saez@brandmanic.com>
2023-12-19 11:04:11 +0100

    Añadido a / acceso directo a token y login

--- routes.js
+++ routes.js
@@ -1,4 +1,11 @@
 module.exports = function (app) {
   app.use("/login", require("./routes/login"));
   app.use("/display", require("./routes/displayapi"));
+  app.use('/', function (req, res) {
+    res.send(`
+    <div style="display:flex;width:100%;height:100%;justify-content:center;align-items:center;font-size:2rem">
+      <a style="background-color:black;color:white;padding:1rem;text-decoration:none;font-weight:600" href="/login" target="_blank">Login</a>
+      <a style="border:1px solid black;padding:1rem;text-decoration:none;font-weight:600:color:black;margin-left:2rem;" href="/login/token" target="_blank">Token</a>
+    </div>`)
+  })
 };
